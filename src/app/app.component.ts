efebbdc92f95021521871659779a70a01c9fecf0
pawellazicki <pawelllaz@gmail.com>
2020-12-15 19:33:45 +0100

    heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'random-numbers';
+  generated = null;
+  values = {
+    min: null,
+    max: null
+  }
+  
+  generate(){
+    if(this.values.max !== null && this.values.min !== null)
+      this.generated = this.getRandomIntInclusive(this.values.min, this.values.max);
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
